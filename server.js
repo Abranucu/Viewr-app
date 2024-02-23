14ed22844e6379d420826ec7514a69e88b48c340
Abranucu <abrahamcastiellobarbas@gmail.com>
2024-02-23 17:51:54 +0100

    routes backend tested

---
+++ server.js
@@ -0,0 +1,22 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+
+const allowsAccessFromAnywhere = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+};
+server.use(allowsAccessFromAnywhere);
+
+const router = jsonServer.router("db.json");
+server.use(router);
+
+const PORT = 5005;
+
+server.listen(PORT, () => {
+  console.log(`Servidor andando! en el puerto ${PORT}`);
+  console.log(`Acceso Local en: http://localhost:${PORT}`);
+});
